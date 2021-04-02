07c4768f9262e25ff46da38bf816295c23f0dede
Dharven Doshi <dharven123@gmail.com>
2021-04-02 15:07:57 +0530

    New

---
+++ app/src/main/java/com/example/ringtone/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ringtone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button start, stop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById( R.id.startButton );
+        stop = (Button) findViewById( R.id.stopButton );
+
+        start.setOnClickListener( this );
+        stop.setOnClickListener( this );
+    }
+    public void onClick(View view) {
+
+        if(view == start){
+
+            startService(new Intent( this, MyService.class ) );
+        }
+
+        else if (view == stop){
+
+            stopService(new Intent( this,  MyService.class ) );
+
+        }
+    }
+}
